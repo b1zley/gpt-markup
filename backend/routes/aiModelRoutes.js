5e675518a9dfba1dbca83b5d259027d7093ed606
b1zley <joshuageardohagan@gmail.com>
2024-07-30 16:56:56 +0100

    swagger: ai_model

--- backend/routes/aiModelRoutes.js
+++ backend/routes/aiModelRoutes.js
@@ -3,7 +3,42 @@ const router = express.Router({ mergeParams: true });
 
 const aiModelControllers = require('../controllers/aiModelControllers')
 
-
+/**
+ * @swagger
+ * /ai_model:
+ *   get:
+ *     security:
+ *       - BearerAuth: []  
+ *     tags:
+ *       - Trained Model
+ *     summary: Retrieves all trained models from the database
+ *     responses:
+ *       '200':
+ *         description: A list of trained models
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   model_id:
+ *                     type: integer
+ *                     description: Unique identifier for the trained model
+ *                   model_name:
+ *                     type: string
+ *                     description: Name of the trained model
+ *                   created_at:
+ *                     type: string
+ *                     format: date-time
+ *                     description: Timestamp when the model was created
+ *                   updated_at:
+ *                     type: string
+ *                     format: date-time
+ *                     description: Timestamp when the model was last updated
+ *       '500':
+ *         description: Internal server error
+ */
 router.get('/', aiModelControllers.getAllHandler )
 
 
