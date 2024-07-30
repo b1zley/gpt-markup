ae20d999f9cab2d53f2edf37b294440209a8dfc7
b1zley <joshuageardohagan@gmail.com>
2024-07-30 13:12:09 +0100

    added start up routes for testing and bundling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
-console.log('Loaded environment variables:', process.env);
+// console.log('Loaded environment variables:', process.env);
 const createApp = require('./app');
-console.log('DB_USER:', process.env.DB_USER); // Should output 'root'
+// console.log('DB_USER:', process.env.DB_USER); // Should output 'root'
 
 const { PORT } = require('./routesCommonDependencies');
 
