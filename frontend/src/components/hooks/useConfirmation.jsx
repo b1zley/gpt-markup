2a8cfcec005fa2686294f4283131087236aac2ff
b1zley <joshuageardohagan@gmail.com>
2024-07-31 13:53:54 +0100

    JSDOC: hooks

--- frontend/src/components/hooks/useConfirmation.jsx
+++ frontend/src/components/hooks/useConfirmation.jsx
@@ -1,6 +1,35 @@
 import { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
+
+/**
+ * `useConfirmation` is a custom React hook that provides a way to display a confirmation modal
+ * and handle user responses. It returns a function to trigger the confirmation modal and a modal
+ * component to be included in the render tree.
+ * 
+ * @returns {[Function, Function]} An array where:
+ *   - The first element is a function `confirm` that triggers the confirmation modal.
+ *   - The second element is a `ConfirmationModal` component that should be included in the render tree.
+ * 
+ * @example
+ * const [confirm, ConfirmationModal] = useConfirmation();
+ * 
+ * const handleAction = async () => {
+ *   const result = await confirm('Are you sure you want to proceed?');
+ *   if (result) {
+ *     // Handle confirmation
+ *   } else {
+ *     // Handle cancellation
+ *   }
+ * };
+ * 
+ * return (
+ *   <div>
+ *     <Button onClick={handleAction}>Trigger Action</Button>
+ *     <ConfirmationModal />
+ *   </div>
+ * );
+ */
 const useConfirmation = () => {
     const [confirmationState, setConfirmationState] = useState({ isOpen: false, resolve: null });
 
